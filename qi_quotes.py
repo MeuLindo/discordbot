bf0027df911ef3a3c4b205df45f883a820dfac72
Al F <alexandreffaria@me.com>
2021-02-25 18:09:15 -0300

    Arquivo separado para organizaçao de quotes

---
+++ qi_quotes.py
@@ -0,0 +1,6 @@
+quotes = [
+        {
+            'Author': 'Stephen Fry',
+            'Quote': 'But perhaps, you know, we should believe in Adam and Eve. Geneticists have established that every woman in the world shares a single female ancestor who lived a hundred and fifty thousand years ago. Scientists actually call her "Eve", and every man shares a single male ancestor called "Adam". It\'s also been established, however, that Adam was born eighty thousand years after Eve. So the world before him was one of heavy to industrial-strength lesbianism, one assumes.'
+        },
+]
