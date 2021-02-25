126df3ff89f09f4df1ddafcebed41fbcd3c61f06
Al F <alexandreffaria@me.com>
2021-02-25 18:08:39 -0300

    Mensagem final formatada e quotes importadas de outro arquivo

--- botlindo.py
+++ botlindo.py
@@ -1,28 +1,22 @@
 import os
 import random
 from dotenv import load_dotenv
-
-# 1
 from discord.ext import commands
+
+from qi_quotes import quotes
 
 load_dotenv()
 TOKEN = os.getenv('DISCORD_TOKEN')
 
-# 2
 bot = commands.Bot(command_prefix='!')
 
-@bot.command(name='99')
-async def nine_nine(ctx):
-    brooklyn_99_quotes = [
-        'I\'m the human form of the 💯 emoji.',
-        'Bingpot!',
-        (
-            'Cool. Cool cool cool cool cool cool cool, '
-            'no doubt no doubt no doubt no doubt.'
-        ),
-    ]
-
-    response = random.choice(brooklyn_99_quotes)
-    await ctx.send(response)
+@bot.command(name='qi')
+async def quite_interesting(ctx):
+    
+    choose = random.choice(quotes)
+    author = '**' + choose['Author'] + ':**'
+    quote = '*' + choose['Quote'] + '*'
+    await ctx.send(author + '\n' + quote)
+    
 
 bot.run(TOKEN)
