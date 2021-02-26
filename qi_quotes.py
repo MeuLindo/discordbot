48e1ce24cf7f7c0075c96bf068a554c4ab37e094
Al F <alexandreffaria@me.com>
2021-02-26 01:01:26 -0300

    +quotes

--- qi_quotes.py
+++ qi_quotes.py
@@ -1,6 +1,28 @@
 quotes = [
         {
             'Author': 'Stephen Fry',
-            'Quote': 'But perhaps, you know, we should believe in Adam and Eve. Geneticists have established that every woman in the world shares a single female ancestor who lived a hundred and fifty thousand years ago. Scientists actually call her "Eve", and every man shares a single male ancestor called "Adam". It\'s also been established, however, that Adam was born eighty thousand years after Eve. So the world before him was one of heavy to industrial-strength lesbianism, one assumes.'
+            'Quote': 'But perhaps, you know, we should believe in Adam and Eve. Geneticists have established that every woman in the world shares a single female ancestor who lived a hundred and fifty thousand years ago. Scientists actually call her "Eve", and every man shares a single male ancestor called "Adam". It\'s also been established, however, that Adam was born eighty thousand years after Eve. So the world before him was one of heavy to industrial-strength lesbianism, one assumes.',
+            'EP': 'A.01'
         },
+        {
+            'Author': 'Alan Davies',
+            'Quote': 'Did you know, kiwi fruit uses more than its own weight in aviation fuel to get from New Zealand to Europe?',
+            'EP': 'A.01'
+        },
+        {
+            'Author': 'Stephen Fry',
+            'Quote': 'Anything you put in frong a Bonobo it will shag, honestly, it doesn\'t matter.',
+            'EP': 'J.10'
+        },
+        {
+            'Author': 'Stephen Fry',
+            'Quote': '8% of all lion sex is gay.',
+            'EP': 'J.10'
+        },
+        {
+            'Author': 'Stephen Fry',
+            'Quote': 'There are, in the latest count, I believe 642 species of animals that exibit homosexual activitie. But there is only one specie that exibits homophobia, and that is Mankind.',
+            'EP': 'J.10'
+        },
+        
 ]
