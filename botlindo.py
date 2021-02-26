906c05d192500947548e2e8ff6e2d05e79f5a93f
Al F <alexandreffaria@me.com>
2021-02-26 01:02:02 -0300

    @botqc

--- botlindo.py
+++ botlindo.py
@@ -2,8 +2,10 @@ import os
 import random
 from dotenv import load_dotenv
 from discord.ext import commands
+import discord
 
 from qi_quotes import quotes
+from qc_quotes import qc
 
 load_dotenv()
 TOKEN = os.getenv('DISCORD_TOKEN')
@@ -16,7 +18,24 @@ async def quite_interesting(ctx):
     choose = random.choice(quotes)
     author = '**' + choose['Author'] + ':**'
     quote = '*' + choose['Quote'] + '*'
-    await ctx.send(author + '\n' + quote)
+    episode = '- Ep: ' + choose['EP']
+    send_message = await ctx.send(author + '\n' + quote + '\n' + episode)
     
+    emoji = '\N{THUMBS UP SIGN}'
+    await send_message.add_reaction(emoji)
+    
+@bot.command(name='qc')
+async def quarentena_gaming(ctx):
+    choose = random.choice(qc)
+    send_message = await ctx.send(choose)
+
+@bot.command(name='smoke')
+async def quarentena_gaming(ctx):
+    await ctx.send('Essa smoke aí é pra ganhar espaço')
+
+@bot.command(name='bomb')
+async def quarentena_gaming(ctx):
+    await ctx.send('O que acontece no bomb fica no bomb.')
+
 
 bot.run(TOKEN)
