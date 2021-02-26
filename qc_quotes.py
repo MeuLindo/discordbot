da73377b5a08473cf9414e3efbabab7c0e69f923
Al F <alexandreffaria@me.com>
2021-02-26 00:59:57 -0300

    Quotes da QC gaming

---
+++ qc_quotes.py
@@ -0,0 +1,10 @@
+qc = [
+    'Espcial QC',
+    'Só tem profissional',
+    'Nasceu pra jogar o jogo',
+    'HE tcholinhas',
+    'Miado: -18',
+    'O que acontece no bomb fica no bomb.',
+    'Essa smoke aí é pra ganhar espaço',
+    'NA utility.',
+]
