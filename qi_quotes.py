d6ea1becaace104431d5bcc33ba58e49a75b2ffa
Al F <alexandreffaria@me.com>
2021-02-26 22:16:14 -0300

    Troca de nomeclatura de eps Num -> Títilo

--- qi_quotes.py
+++ qi_quotes.py
@@ -2,67 +2,77 @@ quotes = [
         {
             'Author': 'Stephen Fry',
             'Quote': 'But perhaps, you know, we should believe in Adam and Eve. Geneticists have established that every woman in the world shares a single female ancestor who lived a hundred and fifty thousand years ago. Scientists actually call her "Eve", and every man shares a single male ancestor called "Adam". It\'s also been established, however, that Adam was born eighty thousand years after Eve. So the world before him was one of heavy to industrial-strength lesbianism, one assumes.',
-            'EP': 'A.01'
+            'EP': 'Adam'
         },
         {
             'Author': 'Alan Davies',
             'Quote': 'Did you know, kiwi fruit uses more than its own weight in aviation fuel to get from New Zealand to Europe?',
-            'EP': 'A.01'
+            'EP': 'Adam'
         },
         {
             'Author': 'Stephen Fry',
             'Quote': 'Anything you put in frong a Bonobo it will shag, honestly, it doesn\'t matter.',
-            'EP': 'J.10'
+            'EP': 'Jungles'
         },
         {
             'Author': 'Stephen Fry',
             'Quote': '8% of all lion sex is gay.',
-            'EP': 'J.10'
+            'EP': 'Jungles'
         },
         {
             'Author': 'Stephen Fry',
             'Quote': 'There are, in the latest count, I believe 642 species of animals that exibit homosexual activitie. But there is only one specie that exibits homophobia, and that is Mankind.',
-            'EP': 'J.10'
+            'EP': 'Jungles'
         },
         {
             'Author': 'Stephen Fry',
             'Quote': 'Elephants only have two knees, the front two are elbows.',
-            'EP': 'J.06'
+            'EP': 'Joints'
         },
         {
             'Author': 'Stephen Fry',
             'Quote': 'Very oddly, the only mammals that have chins, are humans and elephants.',
-            'EP': 'J.06'
+            'EP': 'Joints'
         },
         {
             'Author': 'Stephen Fry',
             'Quote': '...What became known as the Waterloo tooth. Became almost your patriotic duty, if you lost a tooth, to fit in, that of a dead soldier from Waterloo. There was these scavengers, who went around the battle field, pulling out the teeth of the dead bodies, selling them back in barrels, and people would buy them and fit them into the holes where their teeth were and use them.',
-            'EP': 'J.06'
+            'EP': 'Joints'
         },
          {
             'Author': 'Johnny Vegas',
             'Quote': 'You write in the wall with your own feces... people start listings to you.',
-            'EP': 'J.07'
+            'EP': 'Journalism'
         },
         {
             'Author': 'Stephen Fry',
             'Quote': 'Jogging is apparently very good for the memory. It does seems that a few days of running can lead to the growth of hundreds of thousends of new brain cells, in the memory forming part of the brain.',
-            'EP': 'J.08'
+            'EP': 'Jumble'
         },
         {
             'Author': 'Stephen Fry',
             'Quote': '8 hours on the lou, burns as many calories as 1 hour of jogging.',
-            'EP': 'J.08'
+            'EP': 'Jumble'
         },
         {
             'Author': 'Stephen Fry',
             'Quote': 'In Japan it is very traditional, to adopt an adult young man, ages between 25 and 30. Only 2% of adoptees in Japan are infant.',
-            'EP': 'J.05'
+            'EP': 'J-Places'
         },
         {
             'Author': 'Stephen Fry',
             'Quote': 'Yo mama so fat, that she could easily have a care and control diet and regular exercise.',
-            'EP': 'J.15'
+            'EP': 'Jolly'
+        },
+        {
+            'Author': 'Stephen Fry',
+            'Quote': '(On the Manta Shrimp)... They can accelerate throught the water at 10 thousend times the force of gravity, which causes the water in front of them to boil... they\'ve been known to break out of aquarium glass if one strike of their claw.',
+            'EP': 'Invertebrates'
+        },
+        {
+            'Author': 'Stephen Fry',
+            'Quote': 'Nobody knows why moths are attracted to light.',
+            'EP': 'Invertebrates'
         },
         
 ]
